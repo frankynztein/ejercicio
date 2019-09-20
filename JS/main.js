1a195cdb61d1a47f642296919e66ecbab35ada7f
frankynztein <arellanfranciree@gmail.com>
2019-09-20 15:27:43 -0500

    Solución

--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,20 @@
 document.addEventListener('DOMContentLoaded', function(event) {
   // Button Builder
-  var ButtonCreator = function(container) {
+  const ButtonCreator = function(container) {
     this.container = container;
     this.buttons = [];
   };
 
   ButtonCreator.prototype.appendButton = function(number) {
     // Do not change this method, is not part of the test
-    var element = document.createElement('button');
+    const element = document.createElement('button');
     element.innerText = 'Button #' + (number + 1);
     this.container.appendChild(element);
     this.buttons.push(element);
   };
 
   ButtonCreator.prototype.addEventListeners = function() {
-    for (var i = 0; i < this.buttons.length; ++i) {
+    for (let i = 0; i < this.buttons.length; ++i) {
       this.buttons[i].addEventListener('click', function() {
         alert('Button #' + ( i + 1 ) + ' pressed!');
       });
@@ -22,8 +22,8 @@ document.addEventListener('DOMContentLoaded', function(event) {
   };
 
   // Initialization: Use button builder to generate 10 buttons
-  var builder = new ButtonCreator(document.getElementById('container'));
-  for (var i = 0; i < 10; ++i)
+  const builder = new ButtonCreator(document.getElementById('container'));
+  for (let i = 0; i < 10; ++i)
   {
     builder.appendButton(i);
   }
